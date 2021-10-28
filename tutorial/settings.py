e1a4c1410344c2d568cc7bb6dbaae673923b9b91
KMindz <lutchafrederico@gmail.com>
2021-10-28 13:05:54 -0100

    gitignore

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import environ
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,14 +79,7 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tutorial',
-        'USER': 'postgres',
-        'PASSWORD': 'adminlondon01',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+     'default': env.db()
 }
 
 
